db8150c36a720310566d2710746eee5cf9708a22
Akila <akilamatrix@gmail.com>
2017-04-28 23:24:32 +0530

    all done

--- EZReportsWEB/settings.py
+++ EZReportsWEB/settings.py
@@ -23,8 +23,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ""
-S3Connection(os.environ['SECRET_KEY'], os.environ['SECRET_KEY'])
+SECRET_KEY = S3Connection(os.environ['SECRET_KEY'], os.environ['SECRET_KEY'])
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
