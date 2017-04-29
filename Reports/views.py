04e0e77b0126c6056ad5e8ec21141d843b90a2d0
Akila D. Perera <AkilaDPerera@users.noreply.github.com>
2017-04-29 17:02:51 +0530

    Update views.py

--- Reports/views.py
+++ Reports/views.py
@@ -124,8 +124,8 @@ def score(request):
                 #Get enlish value first
                 subjects["subject1"] = ["English", 100]
                 for i in [0, 1, 2, 3]:
-                    print(marks[i].student_id==1)
-                    print(marks[i].student_id)
+                    print(marks[i].subject_id==1)
+                    print(marks[i].subject_id)
                     
                     if (marks[i].subject_id==1):
                         subjects["subject1"] = [Subject.objects.get(id=marks[i].subject_id).name, marks[i].mark]
