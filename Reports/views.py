a7dfe8394b7a2faaff542a00711c179e4d0b81db
Akila D. Perera <AkilaDPerera@users.noreply.github.com>
2017-04-29 16:32:38 +0530

    Update views.py

--- Reports/views.py
+++ Reports/views.py
@@ -124,12 +124,13 @@ def score(request):
                 #Get enlish value first
                 subjects["subject1"] = ["English", 100]
                 for i in [0, 1, 2, 3]:
-                    if (int(marks[i].subject_id)==1):
-                        subjects["subject1"] = [Subject.objects.get(id=marks[i].subject_id).name, marks[i].mark]
-                        s.remove(i)
+                    subjects["subject1"] = [Subject.objects.get(id=marks[i].subject_id).name, marks[i].mark]
+                    #if (int(marks[i].subject_id)==1):
+                        #subjects["subject1"] = [Subject.objects.get(id=marks[i].subject_id).name, marks[i].mark]
+                        #s.remove(i)
                         
                 
-                subjects["subject2"] = [Subject.objects.get(id=marks[s[0]].subject_id).name, str(marks)]#marks[s[0]].mark]
+                subjects["subject2"] = [Subject.objects.get(id=marks[s[0]].subject_id).name, marks[s[0]].mark]
                 subjects["subject3"] = [Subject.objects.get(id=marks[s[1]].subject_id).name, marks[s[1]].mark]
                 subjects["subject4"] = [Subject.objects.get(id=marks[s[2]].subject_id).name, marks[s[2]].mark]
                 
